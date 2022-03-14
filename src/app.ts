e1c50efca7181db3b40bd52f405e6cbaff220765
laiyuknin <laiyuknin@163.com>
2022-03-14 23:00:46 +0800

    feat: 初始化项目

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import { install } from 'offline-plugin/runtime'
+import { Component } from 'react'
+import './app.scss'
+
+if (process.env.TARO_ENV === 'h5') {
+  install()
+}
+
+class App extends Component {
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  // this.props.children 是将要会渲染的页面
+  render () {
+    return this.props.children
+  }
+}
+
+export default App
